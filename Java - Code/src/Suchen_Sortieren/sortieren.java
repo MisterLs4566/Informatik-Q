c7db5c882bd1f4342b7eaf49ace701f249df1acb
MisterLs4566 <85504773+MisterLs4566@users.noreply.github.com>
2022-10-31 01:14:07 +0100

    update

---
+++ Java - Code/src/Suchen_Sortieren/sortieren.java
@@ -0,0 +1,35 @@
+package Suchen_Sortieren;
+
+public class sortieren {
+    public static int[] bubblesort(int[] feld) {
+        System.out.println("leer");
+        return new int[1];
+    }
+
+    public static int[] selectionsort(int[] feld) {
+        System.out.println("leer");
+        return new int[1];
+    }
+
+    public static int[] insertionsort(int[] feld) {
+        System.out.println("leer");
+        return new int[1];
+    }
+
+    public static int[] mergesort(int[] feld) {
+        System.out.println("leer");
+        return new int[1];
+    }
+
+    public static int[] quicksort(int[] feld) {
+        System.out.println("leer");
+        return new int[1];
+    }
+
+    public static void main(String[] args) {
+        int[] zahlenfolge1 = {12, 76, 30, 42, 10, 60, 34, 90, 35};
+        int[] zahlenfolge2 = {76, 43, 91, 20, 44, 55, 21, 77, 30};
+
+        bubblesort(zahlenfolge1);
+    }
+}
