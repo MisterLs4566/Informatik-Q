c787fcb52b653b3f48009e348b6c2d5e2d348335
MisterLs4566 <85504773+MisterLs4566@users.noreply.github.com>
2022-12-09 13:59:26 +0100

    update

--- Java - Stoff/Listen/ADT/src/ADTListe/Liste.java
+++ Java - Stoff/Listen/ADT/src/ADTListe/Liste.java
@@ -8,25 +8,68 @@ public class Liste {
     private int count = 0;
 
     public Liste() {
+
         this.root = null;
         this.current = null;
         this.count = 0;
+
     }
 
     // Einfügen eines Elements vor dem aktuellen Element
     public void insertBefore(Element aElement) {
+        if(this.root == null) {
+            this.root = aElement;
+            this.current = aElement;
+            return;
+        }
+        previous();
+        this.insertBehind(aElement);
+        this.current = aElement;
+
     }
 
     // Einfügen eines Elements nach dem aktuellen Element
     public void insertBehind(Element aElement) {
+
+        if(this.root == null) {
+            this.root = aElement;
+            this.current = aElement;
+            return;
+        }
+        aElement.setNext(this.current.getNext());
+        this.current.setNext(aElement);
+        this.current = aElement;
+
     }
 
     // Anfügen eines Elements an das Ende der Liste
     public void append(Element aElement) {
+
+        if(this.root == null) {
+            this.root = aElement;
+            this.current = aElement;
+            return;
+        }
+        while(this.current.getNext() != null) {
+            this.current = this.current.getNext();
+        }
+
+        this.current.setNext(aElement);
+        this.current = aElement;
+
     }
 
     // Löschen des aktuellen Elements
     public void delete() {
+
+        if(this.root == null) {
+            return;
+        }
+
+        Element scout = this.current.getNext();
+        previous();
+        this.current.setNext(scout);
+
     }
 
     // geht zum ersten Element der Liste
@@ -36,13 +79,16 @@ public class Liste {
 
     // geht zum nächsten Element der Liste
     public void next() {
+
         if(!isLast()) {
             this.current = this.current.getNext();
         }
+
     }
 
     // geht zum vorherigen Element der Liste
     public void previous() {
+
         if(root == this.current) {
             return;
         }
@@ -54,24 +100,33 @@ public class Liste {
         }
 
         this.current = scout;
+
     }
 
     // Test, ob Liste leer ist
     public boolean isEmpty() {
+
         return root == null;
+
     }
 
     // Test, ob aktuelles Element letztes Element der Liste ist
     public boolean isLast() {
+
         return current.getNext() == null;
+
     }
 
     public int getCount() {
+
         return this.count;
+
     }
 
     // gibt das aktuelle Element zurück
     public Element getCurrent() {
+
         return this.current;
+
     }
 }
--- Java - Stoff/Listen/ADT/src/ADTListeD/ListeD.java
+++ Java - Stoff/Listen/ADT/src/ADTListeD/ListeD.java
@@ -15,30 +15,98 @@ public class ListeD {
 
     // Einfügen eines Elements vor dem aktuellen Element
     public void insertBefore(ElementD aElement) {
+
+        if(this.root == null) {
+            this.root = aElement;
+            this.current = aElement;
+            return;
+        }
+
+        this.current = this.current.getPrevious();
+        insertBehind(aElement);
+        this.count ++;
+        this.current = aElement;
+
     }
 
     // Einfügen eines Elements nach dem aktuellen Element
     public void insertBehind(ElementD aElement) {
+
+        if(this.root == null) {
+            this.root = aElement;
+            this.current = aElement;
+            return;
+        }
+
+        aElement.setNext(this.current.getNext());
+        this.current.setNext(aElement);
+
+        aElement.setPrevious(this.current);
+        this.current = this.current.getNext();
+        if(this.current.getNext() != null) {
+            this.current.getNext().setPrevious(aElement);
+        }
+        this.count ++;
+
     }
 
     // Anfügen eines Elements an das Ende der Liste
     public void append(ElementD aElement) {
+
+        if(this.root == null) {
+            this.root = aElement;
+            this.current = aElement;
+            return;
+        }
+        while(this.current.getNext() != null) {
+            this.current = this.current.getNext();
+        }
+
+        this.current.setNext(aElement);
+        aElement.setPrevious(this.current);
+        this.count ++;
+        this.current = this.current.getNext();
+
     }
 
     // Löschen des aktuellen Elements
     public void delete() {
+
+        if(this.root == null || this.current == root)
+        {
+            return;
+        }
+
+        if(this.current.getNext() == null) {
+            this.current = this.current.getPrevious();
+            this.current.setNext(null);
+            return;
+        }
+
+        ElementD scout = this.current.getNext();
+        this.current = this.current.getPrevious();
+        scout.setPrevious(this.current);
+        this.current.setNext(scout);
+
     }
 
     // geht zum ersten Element der Liste
     public void first() {
+        this.current = root;
     }
 
     // geht zum nächsten Element der Liste
     public void next() {
+        if(!isLast()) {
+            this.current = this.current.getNext();
+        }
     }
 
     // geht zum vorherigen Element der Liste
     public void previous() {
+       if(this.current.getPrevious() != null) {
+           this.current = this.current.getPrevious();
+       }
     }
 
     // Test, ob Liste leer ist
